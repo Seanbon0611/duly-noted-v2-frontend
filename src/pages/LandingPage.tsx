125ad738156814bbb61e5fffa5150bd195a9cfe3
Seanbon0611 <seand0611@gmail.com>
2020-12-17 16:29:03 -0800

    added landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,18 @@
+const LandingPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col justify-center w-2/5 text-left border border-black rounded shadow-md h-3/6">
+        <h1>Duly noted info goes here</h1>
+        <p>This would be the space in which we introduce the application</p>
+        <p>I am just filling in test here at the moment</p>
+        <p>
+          Using text-to-speech technology, we're making taking notes down easier
+          than ever!
+        </p>
+        <p>to get started Sign up here!</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
