ea0581323fac32d85ae962a9eb09ad72145d98b0
Seanbon0611 <seand0611@gmail.com>
2020-12-17 20:36:46 -0800

    added signup page

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form action="" className="space-y-6">
+        <label htmlFor="" className="text-sm font-bold">
+          Name:
+        </label>
+        <input
+          type="text"
+          name="user"
+          id=""
+          className="p-2 border border-black rounded"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
