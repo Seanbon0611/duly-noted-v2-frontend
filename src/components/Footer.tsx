dc26b8cc4eea86de3245f6618c2e0211ea8a91d1
Seanbon0611 <seand0611@gmail.com>
2020-12-18 22:20:35 -0800

    changed header/footer bg to a nicer gradient

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-blue-400 h-36">
+    <div className="bg-gradient-to-br from-blue-400 to-indigo-400 h-36">
       <h1>This is the footer content</h1>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between h-24 px-10 bg-blue-400">
+    <div className="flex items-center justify-between h-24 px-10 bg-gradient-to-br from-blue-400 to-indigo-400">
       <div className="flex items-center">
         <Link to="/" className="flex items-center">
           <Logo className="w-16" />
-          <p className="pl-2 text-3xl satisfy">Duly-noted</p>
+          <p className="pl-2 text-3xl satisfy hover:text-white">Duly-noted</p>
         </Link>
         <div className="pl-5">
           <ul className="flex">
