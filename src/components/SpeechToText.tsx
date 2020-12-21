69f05843fb168e572d75bf2367c19aec164fabb3
Seanbon0611 <seand0611@gmail.com>
2020-12-21 15:01:33 -0800

    fixed bug that would submit form when clear note was clicked

--- src/components/SpeechToText.tsx
+++ src/components/SpeechToText.tsx
@@ -4,7 +4,13 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 
 const SpeechToText = () => {
-  const { transcript } = useSpeechRecognition();
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    alert(
+      `You can save this note: "${e.target.text.value}" if you've signed up!`
+    );
+  };
 
   useEffect(() => {
     SpeechRecognition.startListening({ continuous: true });
@@ -19,7 +25,27 @@ const SpeechToText = () => {
   }
   return (
     <div>
-      <textarea name="text" rows={10} cols={50} value={transcript}></textarea>
+      <form className="flex-col" onSubmit={handleSubmit}>
+        <textarea
+          className="flex justify-center border border-black border-solid"
+          name="text"
+          rows={10}
+          cols={50}
+          value={transcript}
+        ></textarea>
+        <div className="flex justify-between w-56 mx-auto">
+          <button type="submit" className="p-2 border border-black">
+            Save note
+          </button>
+          <button
+            type="reset"
+            onClick={resetTranscript}
+            className="p-2 border border-black"
+          >
+            Clear note
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -2,17 +2,11 @@ import React from "react";
 import SpeechToText from "../components/SpeechToText";
 
 const DemoPage = () => {
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(e.target.text.value);
-  };
   return (
     <div className="h-screen">
       <h1 className="flex justify-center text-5xl">Don't Be Shy, Speak Up!</h1>
-      <div className="w-6/12 mx-auto border border-black border-solid ">
-        <form action="" onSubmit={handleSubmit}>
-          <SpeechToText />
-        </form>
+      <div className="w-6/12 mx-auto">
+        <SpeechToText />
       </div>
     </div>
   );
