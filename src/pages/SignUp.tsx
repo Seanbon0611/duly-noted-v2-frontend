b254533e4aac93ae72440c007f3ce2b9ece78d51
Seanbon0611 <seand0611@gmail.com>
2021-01-08 00:10:21 -0800

    added form input component to signup form

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+import FormInput from "../components/FormInput";
 
-const SignUp = () => {
+const SignUp: React.FC = () => {
   return (
-    <div>
-      <h1>Signup</h1>
-      <form action="" className="space-y-6">
-        <label htmlFor="" className="text-sm font-bold">
-          Name:
-        </label>
-        <input
-          type="text"
-          name="user"
-          id=""
-          className="p-2 border border-black rounded"
-        />
-      </form>
+    <div className="h-screen px-10">
+      <h1 className="text-4xl font-bold">Signup</h1>
+      <div className="flex items-center justify-center mx-auto">
+        <form action="" className="space-y-6">
+          <FormInput
+            type="email"
+            label="Email"
+            asterisk={false}
+            textarea={false}
+            placeholder="email@example.com"
+          />
+          <FormInput
+            type="password"
+            label="Password"
+            asterisk={false}
+            textarea={false}
+            placeholder="Password"
+          />
+        </form>
+      </div>
     </div>
   );
 };
