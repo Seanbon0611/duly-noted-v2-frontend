efb554d8f976e5022acedbd2dc93bcb8043b3b4c
Seanbon0611 <seand0611@gmail.com>
2021-01-08 22:47:58 -0800

    add inputs to signup form w/error handling and http post request to register user

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,14 +1,49 @@
+import { useState } from "react";
+import api from "../services/api";
 import FormInput from "../components/FormInput";
 
 const SignUp: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (confirmPassword !== password) {
+      return setError("Password do not match");
+    }
+    const config = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+    api.user.signUp(config).then((data) => console.log(data));
+  };
+
   return (
     <div className="h-screen px-10">
       <h1 className="text-4xl font-bold">Signup</h1>
+      {error ? (
+        <div className="flex justify-center pb-2">
+          <h1 className="text-3xl font-bold text-red-600">{error}</h1>
+        </div>
+      ) : null}
       <div className="flex items-center justify-center mx-auto">
-        <form action="" className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <FormInput
             type="email"
             label="Email"
+            handleChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            name="email"
             asterisk={false}
             textarea={false}
             placeholder="email@example.com"
@@ -16,10 +51,27 @@ const SignUp: React.FC = () => {
           <FormInput
             type="password"
             label="Password"
-            asterisk={false}
-            textarea={false}
+            value={password}
+            handleChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
+            }}
             placeholder="Password"
           />
+          <FormInput
+            type="password"
+            label="Confirm Password"
+            value={confirmPassword}
+            handleChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setConfirmPassword(e.target.value);
+            }}
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+            className="p-2 text-white bg-blue-800 border border-gray-700 rounded hover:bg-blue-500"
+          >
+            Register
+          </button>
         </form>
       </div>
     </div>
