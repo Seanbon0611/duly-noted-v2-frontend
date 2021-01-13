d3ce33c16a1a0545782f218519211bb5161cfb46
Seanbon0611 <seand0611@gmail.com>
2021-01-13 13:07:46 -0800

    added api call function to authenticate user

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 const API_ROOT = "http://localhost:3001/api/v1";
 
-const signUp = async (config: any) => {
+const signUp = async (config: Object) => {
   const response = await fetch(`${API_ROOT}/users/create`, config);
+  return response.json();
+};
+
+const login = async (config: Object) => {
+  const response = await fetch(`${API_ROOT}/login`, config);
   return response.json();
 };
 
@@ -9,5 +14,8 @@ export default {
   API_ROOT: API_ROOT,
   user: {
     signUp: signUp,
+  },
+  auth: {
+    login: login,
   },
 };
