2f36cc1ed7f5f0c09245e2794dabac21016c6958
Seanbon0611 <seand0611@gmail.com>
2021-01-12 22:24:07 -0800

    created Signin route and updated component with form inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 import Footer from "./components/Footer";
-import "./assets/output.css";
 import SignUp from "./pages/SignUp";
 import DemoPage from "./pages/DemoPage";
 import ContactPage from "./pages/ContactPage";
+import SignIn from "./pages/SignIn";
+import "./assets/output.css";
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
         <Route path="/demo" component={DemoPage} />
         <Route path="/contact" component={ContactPage} />
       </Switch>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { useState } from "react";
+import FormInput from "../components/FormInput";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className="h-screen">
+      <h1 className="text-5xl">Sign In</h1>
+      <FormInput label="Email" type="email" value={email} />
+      <FormInput label="Password" type="password" value={password} />
     </div>
   );
 };
