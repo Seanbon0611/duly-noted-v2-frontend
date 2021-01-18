389672f3173e6cf4fa9a2abe6bb2a012252c907f
Seanbon0611 <seand0611@gmail.com>
2021-01-18 10:22:02 -0800

    added array of objects that will hold social media icons & links

---
+++ src/socials.tsx
@@ -0,0 +1,29 @@
+const socials = [
+  {
+    title: "Medium",
+    link: "https://sdever.medium.com/",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/medium.svg",
+    alt: "medium",
+  },
+  {
+    title: "Linkedin",
+    link: "https://www.linkedin.com/in/sean-dever/",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg",
+    alt: "linkedin",
+  },
+  {
+    title: "Email",
+    link: "mailto:seandever.developer@gmail.com",
+    icon: "https://image.flaticon.com/icons/png/512/666/666162.png",
+    alt: "email",
+  },
+  {
+    title: "Github",
+    link: "https://github.com/Seanbon0611/",
+    icon:
+      "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+    alt: "github",
+  },
+];
+
+export default socials;
