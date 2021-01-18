31d8442d02340440eaac4c691ac1aeab108df2f6
Seanbon0611 <seand0611@gmail.com>
2021-01-18 10:22:26 -0800

    centered textarea and other text

--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -4,9 +4,12 @@ import SpeechToText from "../components/SpeechToText";
 const DemoPage = () => {
   return (
     <div className="h-screen">
-      <h1 className="flex justify-center text-5xl">Don't Be Shy, Speak Up!</h1>
-      <div className="w-6/12 mx-auto">
-        <SpeechToText />
+      <h1 className="text-4xl">Demo</h1>
+      <div className="flex flex-col justify-center mx-auto text-center">
+        <h1 className="py-4 text-5xl">Don't Be Shy, Speak Up!</h1>
+        <div className="mx-auto">
+          <SpeechToText />
+        </div>
       </div>
     </div>
   );
