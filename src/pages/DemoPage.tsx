0c62124e7f8fb8e34e3c5fb56eed6c54d10714db
Seanbon0611 <seand0611@gmail.com>
2021-01-20 12:57:20 -0800

    adjusted styling to format to screen

--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -4,7 +4,7 @@ import SpeechToText from "../components/SpeechToText";
 const DemoPage = () => {
   return (
     <div className="h-screen">
-      <h1 className="text-4xl">Demo</h1>
+      <h1 className="px-10 text-4xl">Demo</h1>
       <div className="flex flex-col justify-center mx-auto text-center">
         <h1 className="py-4 text-5xl">Don't Be Shy, Speak Up!</h1>
         <div className="mx-auto">
