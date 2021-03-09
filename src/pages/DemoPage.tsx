14e151bc973d79d3537d1c40076eaaf8c66249a5
Seanbon0611 <seand0611@gmail.com>
2021-03-08 18:36:37 -0800

    updated these two pages to contain the SPeech Recog package as they need the transcript in the POST request

--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import SpeechToText from "../components/SpeechToText";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const DemoPage = () => {
+  const commands = [
+    {
+      command: "clear",
+      callback: ({ resetTranscript }: any) => resetTranscript(),
+    },
+  ];
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
   const handleSubmit = (e: any) => {
     e.preventDefault();
     alert(
@@ -14,7 +24,12 @@ const DemoPage = () => {
       <div className="flex flex-col pt-20 mx-auto text-center">
         <h1 className="py-4 text-5xl">Don't Be Shy, Speak Up!</h1>
         <div className="mx-auto">
-          <SpeechToText handleSubmit={handleSubmit} />
+          <SpeechToText
+            handleSubmit={handleSubmit}
+            transcript={transcript}
+            resetTranscript={resetTranscript}
+            SpeechRecognition={SpeechRecognition}
+          />
         </div>
       </div>
     </div>
--- src/pages/NewNotePage.tsx
+++ src/pages/NewNotePage.tsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 import SpeechToText from "../components/SpeechToText";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import api from "../services/api";
 
-type Props = {
-  id: number;
-};
-
-const NewNotePage: React.FC<Props> = ({ id }) => {
-  console.log(id);
+const NewNotePage = () => {
   const [content, setContent] = useState("");
+  const userId = localStorage.getItem("user_id");
+  const commands = [
+    {
+      command: "clear",
+      callback: ({ resetTranscript }: any) => resetTranscript(),
+    },
+  ];
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const config = {
       method: "POST",
-      credentials: "include",
-      "Access-Control-Allow-Credentials": "true",
       headers: {
         accept: "application/json",
-        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        id: id,
-        content: content,
+        user_id: parseInt(userId || "{}"),
+        content: transcript,
       }),
     };
     api.note.newNote(config).then(console.log);
@@ -32,7 +35,13 @@ const NewNotePage: React.FC<Props> = ({ id }) => {
       <div className="flex flex-col pt-20 mx-auto text-center">
         <h1 className="py-4 text-5xl">Don't Be Shy, Speak Up!</h1>
         <div className="mx-auto">
-          <SpeechToText handleSubmit={handleSubmit} setContent={setContent} />
+          <SpeechToText
+            transcript={transcript}
+            SpeechRecognition={SpeechRecognition}
+            resetTranscript={resetTranscript}
+            handleSubmit={handleSubmit}
+            setContent={setContent}
+          />
         </div>
       </div>
     </div>
